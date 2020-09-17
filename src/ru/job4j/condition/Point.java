8a06dab893756954702a77d69fc8c7b75a8da06e
Dmitriy Yugai <studentnstu97@mail.ru>
2020-09-17 22:22:44 +0700

    9.2. Checkstyle [#358022]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -5,7 +5,7 @@ import static java.lang.Math.*;
 public class Point {
 
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = sqrt(pow(x2 - x1,2) + pow(y2 - y1,2));
+        double rsl = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
         return rsl;
     }
 
