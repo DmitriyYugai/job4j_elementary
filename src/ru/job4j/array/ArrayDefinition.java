3d01af224e884c9152582dac905f6029649195b1
Dmitriy Yugai <studentnstu97@mail.ru>
2020-09-27 14:12:05 +0700

    6.0.2. Заполнение массива. [#357989]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Korobeinikov Stas";
+        names[2] = "Andrei Hincu";
+        names[3] = "Dmitry Yugay";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
