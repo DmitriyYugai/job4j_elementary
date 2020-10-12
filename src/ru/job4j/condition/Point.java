925626594c61460c84bac9d8c598ca8bae6743ea
Dmitriy Yugai <studentnstu97@mail.ru>
2020-10-12 17:42:22 +0700

    4. Расстояние между точками в трехмерном пространстве. [#358164]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -6,14 +6,33 @@ public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point point) {
         return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
+    public double distance3d(Point point) {
+        return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2) + pow(z - point.z, 2));
+    }
+
+    public void info3d() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 
     public static void main(String[] args) {
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -32,4 +32,32 @@ public class PointTest {
         double out = a.distance(b);
         assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void when123to456then5() {
+        double expected = 5.19;
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(4, 5, 6);
+        double out = a.distance3d(b);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when153to164then1() {
+        double expected = 1.41;
+        Point a = new Point(1, 5, 3);
+        Point b = new Point(1, 6, 4);
+        double out = a.distance3d(b);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when253to674then4() {
+        double expected = 4.58;
+        Point a = new Point(2, 5, 3);
+        Point b = new Point(6, 7, 4);
+        double out = a.distance3d(b);
+        assertEquals(expected, out, 0.01);
+    }
+
 }
